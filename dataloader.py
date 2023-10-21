1fc60fab9db64625c78ea2af99ce12045470aeb5
K-Hooshanfar <83825004+K-Hooshanfar@users.noreply.github.com>
2023-10-21 16:16:07 +0330

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,48 +1,46 @@
-# This file contains the data loader used to read images and annotations from the Custom dataset.
-
-import random
-import pathlib as pl
+# Two arguments are needed to load image files: one is the root of directory,
+#                                               the other is a txt file containing filepath and label.
+# The created ImageFile object can be passed to a pytorch DataLoader for multi-threading process.
+#
+# Author : Sen Jia 
+#
 
 import torch.utils.data as data
+
 from PIL import Image
-from scipy import io
+from PIL import ImageFilter
+import os
+import os.path
+import numpy as np
+import scipy.misc as misc
+import torch
+import torchvision.transforms as transforms
 
-def make_trainset(root):
-    if not isinstance(root, pl.Path):
-        root = pl.Path(root)
-        
-    img_root = root / "train/train_stimuli"
-    fix_root = root / "train/train_fixation"
-    map_root = root / "train/train_saliency"
+from random import randint
+import random
 
-    files = [f.stem for f in img_root.glob("*.jpg")]
-    print(len(files))
-
+def make_dataset(root,txt_file):
     images = []
-    i=0
-    for f in files:
-        print(i)
-        img_path = (img_root / f).with_suffix(".jpg")
-        print(img_path)
-        fix_path = (fix_root / (f + "_fixPts.png")).with_suffix(".png")
-        print(fix_path)
-        map_path = (map_root / (f + "_fixMap")).with_suffix(".jpg")
-        print(map_path)
-        i +=1 
-        images.append([img_path, fix_path, map_path])
-
+    with open(txt_file,"r") as f:
+        for line in f:
+            strs = line.rstrip("\n").split(" ")
+            images.append((os.path.join(root,strs[0]), os.path.join(root, strs[1]),os.path.join(root, strs[2])))
     return images
 
 def pil_loader(path):
     return Image.open(path).convert('RGB')
 
+def map_loader(path):
+    return Image.open(path).convert('L')
 
 def accimage_loader(path):
     import accimage
     try:
         return accimage.Image(path)
     except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
         return pil_loader(path)
+
 
 def default_loader(path):
     from torchvision import get_image_backend
@@ -51,66 +49,42 @@ def default_loader(path):
     else:
         return pil_loader(path)
 
-def map_loader(path):
-    return Image.open(path).convert('L')
-
-def mat_loader(path, shape):
-    mat = io.loadmat(path)["gaze"]
-    fix = []
-    for row in mat:
-        data = row[0].tolist()[2]
-        for p in data:
-            if p[0]<shape[0] and p[1]<shape[1]: # remove noise at the boundary.
-                fix.append(p.tolist())
-    return fix
-
 class ImageList(data.Dataset):
-    def __init__(self, root, transform=None, train=False,
-                 loader=default_loader, mat_loader=mat_loader, map_loader=map_loader):
-
-        imgs = make_trainset(root)
-        print(root)
+    def __init__(self, root, txt_file, transform=None, target_transform=None, map_size=None,
+                 loader=default_loader, map_loader=map_loader, size_out=None, aug=False):
+        imgs = make_dataset(root, txt_file)
         if not imgs:
-            raise(RuntimeError("Found 0 images in folder: " + str(root) + "\n"))
+            raise(RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                               "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
 
         self.root = root
         self.imgs = imgs
         self.transform = transform
-        self.train = train
+        self.target_transform = target_transform
         self.loader = loader
         self.map_loader = map_loader
-        self.mat_loader = mat_loader
+        self.mid = False
 
     def __getitem__(self, index):
+        def _post_process(smap):
+            smap = smap - smap.min()
+            smap = smap / smap.max()
+            return smap
 
         img_path, fix_path, map_path = self.imgs[index]
 
         img = self.loader(img_path)
         w, h = img.size
-        fixmap = self.loader(fix_path)
-        smap = self.map_loader(map_path)
 
-        # fixmap = self.pts2pil(fixpts, img)
-
-        if self.train:
-            if random.random() > 0.5:
-                img = img.transpose(Image.FLIP_LEFT_RIGHT)
-                smap = smap.transpose(Image.FLIP_LEFT_RIGHT)
-                fixmap = fixmap.transpose(Image.FLIP_LEFT_RIGHT)
-
+        s_map = self.map_loader(map_path)
 
         if self.transform is not None:
             img = self.transform(img)
-            smap = self.transform(smap)
-            fixmap = self.transform(fixmap)
-        return img, fixmap, smap
+            s_map = self.transform(s_map)
 
-    def pts2pil(self, fixpts, img):
-        fixmap = Image.new("L", img.size)
-        for p in fixpts:
-            fixmap.putpixel((p[0], p[1]), 255)
-        return fixmap
+        s_map = _post_process(s_map)
+
+        return img, s_map,
 
     def __len__(self):
         return len(self.imgs)
-
